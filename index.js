474a54945e816b598cff261e3290cbddb68471f5
Andrea Wu <andrea.wzg@gmail.com>
2024-03-12 11:43:03 -0700

    added color palette and Kanit font to css file

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
+
 const app = express();
 const port = 3000;
-app.set('view engine', 'ejs');
+
+app.set('view engine', 'ejs');;
+app.use(express.static("public"));
 
 app.get('/', (req, res) => {
   res.render('index', {
-      title: "Home Page"
+      title: "My Site"
   })
 })
 
